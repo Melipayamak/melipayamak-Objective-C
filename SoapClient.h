a23cd869d06fa6700fef30cba7be2857d7c02366
Amirhossein Mehrvarzi <36813422+triggergroup@users.noreply.github.com>
2018-05-26 08:22:42 +0430

    Update SoapClient.h

--- SoapClient.h
+++ SoapClient.h
@@ -1,45 +1,79 @@
 //
+
 //  SoapClient.h
-//  MeliPayamak
-//
-//  Created by Amirhossein Mehrvarzi on 4/24/18.
-//  Copyright © 2018 MeliPayamak. All rights reserved.
+
+//  mp_objC
+
 //
 
+//  Created by Amirhossein Mehrvarzi on 4/24/18.
+
+//  Copyright © 2018 Amirhossein Mehrvarzi. All rights reserved.
+
+//
+
+
 #ifndef SoapClient_h
+
 #define SoapClient_h
+
 
 
 #endif /* SoapClient_h */
 
+
 @interface SoapClient : NSObject
 
+
 @property NSMutableData *mutableData;
+
 @property NSString *response;
+
+@property NSString *sendingElementName;
+
 @property NSString *currentElementName;
+
 @property NSString *expectedElementName;
 
+
 @property NSString *_sendEndpoint;
+
 @property NSString *_receiveEndpoint;
+
 @property NSString *_usersEndpoint;
+
 @property NSString *_contactsEndpoint;
+
 @property NSString *_actionsEndpoint;
+
 @property NSString *_scheduleEndpoint;
 
+
 @property NSString *username;
+
 @property NSString *password;
 
+
 -(void)SendSimpleSMS2: (NSString *) to
+
                sender: (NSString *) from
+
                   msg: (NSString *) message
+
                 flash: (BOOL) isFlash;
 
+
 -(id)initCred: (NSString *) aUsername
+
      password: (NSString *) aPassword;
 
+
 -(void)initAndSendRequest: (NSString *) endpoint
-				  	  msg: (NSString *) message;
+
+                      msg: (NSString *) message;
+
 
 //place other methods which you are using here
+
 
 @end
